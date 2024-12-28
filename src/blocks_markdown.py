231575a300d8dce72784e398ea476b4e8c9e945f
dangoodie <dan.gooden.dev@gmail.com>
2024-12-29 01:22:08 +1100

    Most of block type identification is complete.

---
+++ src/blocks_markdown.py
@@ -0,0 +1,49 @@
+import re
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def markdown_to_blocks(markdown):
+
+    # Split the markdown by two or more newlines
+    blocks = re.split(r"\n\s*\n", markdown)
+
+    # Remove leading and trailing whitespace from each block
+    blocks = [block.strip() for block in blocks if block.strip()]
+
+    return blocks
+
+
+def block_to_block_type(block):
+
+    # Match for headings
+    heading_regex = r"#{1,6} "
+    if re.match(heading_regex, block) and len(block) > (block.find(" ") + 1):
+        return BlockType.HEADING
+
+    # Match for code blocks
+    lines = block.split("\n")
+    if lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+
+    # Match for quotes
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+
+    # Match for unordered lists
+    if all(line.startswith("* ") or line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+
+    # Match for ordered lists
+    # TODO Build ordered lists logic
+
+    # if none it must be paragraph!
+    return BlockType.PARAGRAPH
---
+++ src/test_blocks_mardown.py
@@ -0,0 +1,80 @@
+import unittest, textwrap
+
+from blocks_markdown import *
+
+
+class TestBlocksMarkdown(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        markdown = textwrap.dedent(
+            """\
+        # This is a heading
+
+        This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+        * This is the first list item in a list block
+        * This is a list item
+        * This is another list item
+        """
+        )
+
+        blocks = markdown_to_blocks(markdown)
+
+        self.assertEqual(blocks[0], "# This is a heading")
+        self.assertEqual(
+            blocks[1],
+            "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+        )
+        self.assertEqual(
+            blocks[2],
+            """* This is the first list item in a list block\n* This is a list item\n* This is another list item""",
+        )
+
+    def test_block_to_block_type_heading(self):
+        block = "###### This is a heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_block_type_heading2(self):
+        block = "### "
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_block_type_heading3(self):
+        block = "###No Space"
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_block_type_heading4(self):
+        block = "a ### wow"
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_block_type_code(self):
+        block = "```\nsome code\n```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_block_type_code2(self):
+        block = "```just one line```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_block_type_quote(self):
+        block = ">This is the first line\n>This is the second line"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_block_to_block_type_ul(self):
+        block = "- This is the first line\n- This is the second line"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type_ul2(self):
+        block = "* This is the first line\n* This is the second line"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type_ul3(self):
+        block = "* This is the first line\n- This is the second line"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)

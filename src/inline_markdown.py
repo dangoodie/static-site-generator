58ce19c8e683582df6125c6a1364de2b7f4aacc1
dangoodie <dan.gooden.dev@gmail.com>
2024-12-28 23:02:33 +1100

    Text to textnodes now working with tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -19,6 +19,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
     return new_nodes
 
+
 def extract_markdown_images(text):
     pattern = r"!\[(.*?)\]\((.*?)\)"
     matches = re.findall(pattern, text)
@@ -37,51 +38,90 @@ def extract_markdown_links(text):
     for match in matches:
         if len(match) != 2:
             raise ValueError("Invalid link")
-        
+
     return matches
+
 
 def split_nodes_image(old_nodes):
     new_nodes = list()
+
     for node in old_nodes:
         matches = extract_markdown_images(node.text)
+
+        # Continue if no matches
+        if not matches:
+            new_nodes.append(node)
+            continue
+
         remaining_text = node.text
         for match in matches:
+            # Construct the markdown text
             markdown_image_str = f"![{match[0]}]({match[1]})"
+
+            # Split the text into two parts
             parts = remaining_text.split(markdown_image_str, 1)
 
-            # add the text before the image if not empty
-            if parts[0] != "":
-                new_nodes.append(TextNode(parts[0], TextType.NORMAL))
+            # Add the text before the image if not empty
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], node.text_type))
+
+            # Add the image node
             new_nodes.append(TextNode(match[0], TextType.IMAGE, match[1]))
+
+            # Update the remaining text
             remaining_text = parts[1]
-        
-        # add the remaining text if not empty
-        if remaining_text != "":
-            new_nodes.append(TextNode(remaining_text, TextType.NORMAL))
+
+        # Add the remaining text if not empty
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text, node.text_type))
 
         return new_nodes
+
 
 def split_nodes_link(old_nodes):
     new_nodes = list()
+
     for node in old_nodes:
         matches = extract_markdown_links(node.text)
+
+        # Continue if no matches
+        if not matches:
+            new_nodes.append(node)
+            continue
+
         remaining_text = node.text
+
         for match in matches:
+            # Construct the markdown link
             markdown_link_str = f"[{match[0]}]({match[1]})"
+
+            # Split the text into 2 parts
             parts = remaining_text.split(markdown_link_str, 1)
 
             # add the text before the link if not empty
-            if parts[0] != "":
-                new_nodes.append(TextNode(parts[0], TextType.NORMAL))
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], node.text_type))
+
+            # Add the link node
             new_nodes.append(TextNode(match[0], TextType.LINK, match[1]))
+
+            # Update the remaining part
             remaining_text = parts[1]
-        
+
         # add the remaining text if not empty
-        if remaining_text != "":
-            new_nodes.append(TextNode(remaining_text, TextType.NORMAL))
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text, node.text_type))
 
         return new_nodes
 
 
-def reconstruct_markdown_image_str(text, url):
-    return f"![{text}]({url})"
+def text_to_textnodes(text):
+    nodes = split_nodes_delimiter(
+        [TextNode(text, TextType.NORMAL)], "**", TextType.BOLD
+    )
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+
+    return nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -205,6 +205,27 @@ class TestInlineMarkdown(unittest.TestCase):
         )
         self.assertEqual(nodes[4], TextNode(" and even more text.", TextType.NORMAL))
 
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        textnodes = text_to_textnodes(text)
+        self.assertEqual(
+            [
+                TextNode("This is ", TextType.NORMAL),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.NORMAL),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.NORMAL),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.NORMAL),
+                TextNode(
+                    "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+                ),
+                TextNode(" and a ", TextType.NORMAL),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            textnodes,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
